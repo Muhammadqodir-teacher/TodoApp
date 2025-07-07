8b03d8c37d85dadea30d3e29e62aeca3bf1a70d0
Muhammadqodirgit <muhammadqodirov3609@gmail.com>
2025-07-07 13:08:44 +0500

    data check input

--- script.js
+++ script.js
@@ -11,56 +11,79 @@ const modalBtnNo = document.querySelector(".modal__nobtn");
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 });
-
 BtnAdd.addEventListener("click", () => {
     if (data.value === "") {
         alert("Enter something");
     } else {
+        // element list
         const element = document.createElement("div");
         element.className = "element";
         collect.prepend(element);
-
-
+        // checkBox and text
+        const elementText = document.createElement("span");
+        elementText.classList = "elementText";
+        element.prepend(elementText);
+        // check input
+        const complate = document.createElement("input");
+        complate.setAttribute("type", "checkbox");
+        elementText.appendChild(complate);
+        // check input event
+        complate.addEventListener('change', () => {
+            if (complate.checked) {
+                text.style.textDecoration = "line-through"
+            } else {
+                text.style.textDecoration = "none"
+            }
+        })
+        // text list
         const text = document.createElement("p");
         text.classList = "text"
         text.textContent = data.value;
-        element.prepend(text);
-
+        elementText.appendChild(text);
+        // data time
+        const time = document.createElement("p");
+        let localData = new Date();
+        let hours = (localData.getHours());
+        let minutes = (localData.getMinutes());
+        let seconds = (localData.getSeconds());
+        time.textContent = `${hours} : ${minutes} : ${seconds}`
+        element.appendChild(time)
+        // edit dalet icons div
         const elementBox = document.createElement("div");
         elementBox.classList = "elementBox";
         element.appendChild(elementBox);
-
+        // dalete icon
         const deleteIcon = document.createElement("i");
         deleteIcon.className = "fa-solid fa-delete-left";
         elementBox.appendChild(deleteIcon);
-
+        // edit icon
         const editIcon = document.createElement("i");
         editIcon.className = "fa-solid fa-pen-to-square";
         elementBox.prepend(editIcon);
-
+        // check icon
         const checkIcon = document.createElement("i");
         checkIcon.className = "fa-solid fa-check";
         checkIcon.style.display = "none";
         elementBox.prepend(checkIcon);
-
+        // edit new input
         const newInput = document.createElement("input");
         newInput.className = "newInput";
         newInput.style.display = "none";
         element.prepend(newInput);
-
+        // dalete all event
         daleteAll.addEventListener('click', () => {
             modal.style.display = "flex";
         });
-
+        // modal btns yes
         modalBtnYes.addEventListener('click', () => {
             collect.innerHTML = "";
             modal.style.display = "none"
         });
-
+        // modal btns no
         modalBtnNo.addEventListener('click', () => {
             modal.style.display = "none"
         });
-
+        // edit icon event
         editIcon.addEventListener("click", () => {
             const oldText = element.childNodes[1].textContent;
             newInput.value = oldText;
@@ -70,7 +93,7 @@ BtnAdd.addEventListener("click", () => {
             checkIcon.style.display = "block";
             editIcon.style.display = "none";
         });
-
+        // check icon event
         checkIcon.addEventListener("click", () => {
             if (newInput.value.trim() !== "") {
                 element.childNodes[1].textContent = newInput.value;
@@ -81,21 +104,22 @@ BtnAdd.addEventListener("click", () => {
             checkIcon.style.display = "none";
             editIcon.style.display = "block";
         });
-
-
-
+        // dalete icon text remove
         deleteIcon.addEventListener("click", () => {
             element.remove();
         });
 
     }
+    // data input value
     data.value = "";
-
-
-
+    // modal display none window
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none"
+        }
+    }
 });
-
-
+// search function
 function searchFunction() {
     let filter = searech.value.toUpperCase();
     let elements = document.querySelectorAll(".element");
